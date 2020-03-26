65a98b948e94e91391bc4456e67691d6031ffe43
Sanket Kale <55947684+Sank-Infinity@users.noreply.github.com>
2020-03-26 14:17:48 +0530

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,34 +1,62 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Mar 21 13:45:33 2020
 
 @author: Sanket Kale
 """
-#Importing the libraries
+#Importing The Libraries
 import numpy as np
 import pandas as pd 
 
-#Importing the DataSet
+#Importing the Dataset
 dataset = pd.read_csv('corona.csv')
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,10].values
 
-#Splitting the DataSet into training Set and Testing Set
+#Spliting the Dataset as TrainingSet and TestSet
 from sklearn.cross_validation import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=1/5, random_state=0)
 
-#Importing the Regression model
+#Appling Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+X_train = sc_x.fit_transform(X_train)
+X_test = sc_x.fit_transform(X_test)
+
+#Impoting The Classifier 
 from sklearn.linear_model import LogisticRegression
 cls = LogisticRegression(random_state=0)
 cls.fit(X_train, Y_train)
 
-#Probability prediction 
+#Prediction of results for X_test
 Y_pred = cls.predict(X_test)
 
-#cross_verifing the real values with pridected values
+#Calculation of confusion Matrix for correct prediction
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_pred)
 
-#sample Example with array of SYMPTOMS [ Fever,Dry cough,.......,Pain in chest,age ] in the form of -1/0/1 excluding fever and age.
-prob_infection =cls.predict_proba([98,0,0,0,0,1,0,0,-1,22])
+#Taking Input form user for prediction 
+spt = []
+spt.append(int(input('Fever in fehrenheit:\n ')))
+spt.append(int(input('Dry cough : (Yes = 1 or No=0)\n')))
+spt.append(int(input('Difficulty in breathing :(Yes = 1 or mild = 0 or No= -1)\n')))
+spt.append(int(input('RunnyNose : (Yes = 1 or No=0)\n')))
+spt.append(int(input('Tiredness :(Yes = 1 or No=0)\n')))
+spt.append(int(input('BodyAches : (Yes = 1 or mild = 0 or No= -1)\n')))
+spt.append(int(input('Sore Throat : (Yes = 1 or mild = 0 or No= -1)\n')))
+spt.append(int(input('Bluish lips or Face :(Yes = 1 or No=0)\n')))
+spt.append(int(input('Persistent pain or pressure in the Chest :(Yes = 1 or mild = 0 or No= -1)\n')))
+spt.append(int(input('Age : ')))
 
+#Results of Input
+prob_infection =cls.predict_proba([spt])
+print ("Probability tHat you are CORONA POSITIVE :" , prob_infection[:,[1]])
+print ("Probability tHat you are CORONA NEGATIVE :" , prob_infection[:,[0]])
+
+
+#converting List in to 2D array
+spt = np.asarray([spt])
+numpyarray = spt
+
+#tabular Form of symptoms status of patient
+df = pd.DataFrame({'Age' : numpyarray[:, 9],'Pain_in_chest' : numpyarray[:, 8],'Bluish_lips' : numpyarray[:, 7],'SoreThroat' : numpyarray[:, 6],'BodyAches' : numpyarray[:, 5],'Tiredness' : numpyarray[:, 4],'RunnyNose' : numpyarray[:, 3],'Diff_in_Breath' : numpyarray[:, 2], 'Dry_cough': numpyarray[:,1], 'Fever' : numpyarray[:, 0]})
+print(df)
